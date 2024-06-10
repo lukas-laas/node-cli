5bb66d80de0d4f81d4d3819d05e5eb383c370605
lukas-laas <lukas.s.laas@gmail.com>
2024-06-10 10:19:03 +0200

    add help

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,6 @@
 #! /usr/bin/env node
 const args = process.argv;
+if (args[2] == "-help" || args[2] == "-h")
+  return console.log('Run: simpcli "name" to greet yourself');
+
 console.log(`Hello ${args[2] ? args[2] : "world"}!`);
